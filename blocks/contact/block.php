4d8092ed57e879c3abdc05a886fd7d641d66bce0
Arturo Valverde <art.valverde.563@gmail.com>
2025-02-27 00:35:43 -0600

    Cambios en el subrrayado del bloque de 3 columnas, en el formato de los encabezados y se agregaron telefono y correo al bloque de contacto

--- blocks/contact/block.php
+++ blocks/contact/block.php
@@ -13,12 +13,20 @@ if (get_field('mostrar_bloque')):
 
                     <<?= $etiqueta_del_titulo ?> class="block-title"><?= $titulo ?></<?= $etiqueta_del_titulo ?>>
 
+                    <?php if (isset($correo_electronico) && $correo_electronico !== null): ?>
+                        <a href="mailto:<?= $correo_electronico ?>" class="contact"><?= $correo_electronico ?></a>
+                    <?php endif ?>
+
+                    <?php if (isset($telefono) && $telefono !== null): ?>
+                        <a href="tel:+52<?= preg_replace('/^0|[^a-zA-Z0-9+]+/', '', $telefono) ?>" class="contact"><?= $telefono ?></a>
+                    <?php endif ?>
+
                 </div>
 
                 <div class="col">
 
                     <div class="form">
-                        <?= do_shortcode(get_field('shortcode_de_formulario')) ?>
+                        <?= do_shortcode($shortcode_de_formulario) ?>
                     </div>
 
                 </div>
